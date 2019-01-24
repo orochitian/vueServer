25f7d55ddecbcba5a75b29147420900ffacc93fc
orochitian <orochitian@126.com>
2019-01-24 17:32:30 +0800

    用户登录

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 var app = require('express')();
 var mongo = require('mongoose');
 var bodyParser = require('body-parser');
-var sidebar = require('./sidebar');
 var userModel = require('./model/userModel');
+var session = require('express-session');
 
 app.use('*', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8081");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Content-Type", "application/json;charset=utf-8");
-    next()
+    next();
 });
 
 app.use( bodyParser.urlencoded({extended : true}) );
 app.use( bodyParser.json() );
-
-app.use('/blog', require('./routers/blog'));
-
-//  获取左侧导航
-app.get('/getSidebar', (req, res) => {
-    res.send(sidebar)
-});
+app.use(session({
+    secret : 'ryan lee',
+    resave : true,
+    saveUninitialized : true
+}));
 
 //  登录
 app.post('/login', (req, res) => {
@@ -31,86 +30,30 @@ app.post('/login', (req, res) => {
         if( user.length < 1 ) {
             res.send(false);
         } else {
+            req.session.sign = true;
             res.send(true);
         }
     });
 });
 
-//  获取用户列表
-app.get('/user/getUserList', (req, res) => {
-    var pageNum = req.query.pageNum * 1 || 1;
-    var pageSize = req.query.pageSize * 1 || 10;
-    userModel.find({}).estimatedDocumentCount().then(count => {
-        if( count === 0 ) {
-            res.json({
-                users: [],
-                pageNum: 1,
-                pageSize,
-                count
-            });
-            return;
-        }
-        if( pageSize * pageNum > count ) {
-            pageNum = Math.ceil(count / pageSize);
-        }
-        userModel.find().skip((pageNum-1) * pageSize).limit(pageSize).then(users => {
-            res.json({
-                users,
-                pageNum: pageNum || 1,
-                pageSize,
-                count
-            });
-        });
-    });
+//  登出
+app.get('/logout', (req, res) => {
+    req.session.sign = false;
+    res.send(true);
 });
 
-//  获取指定用户信息
-// app.get('/user/getUserById', (req, res) => {
-//     userModel.findById(req.query._id, (err, user) => {
-//         if( !err ) {
-//             res.json(user);
-//         } else {
-//             res.status(401).send('用户未找到！');
-//         }
-//     });
-// });
-
-//  添加用户
-app.post('/user/addUser', (req, res) => {
-    userModel.find({username: req.body.username}).then(user => {
-        if( user.length < 1 ) {
-            userModel.create(req.body, err => {
-                res.send(true);
-            });
-        } else {
-            res.send(false);
-        }
-    });
-
+app.use((req, res, next) => {
+    console.log(req.sessionID);
+    if( !req.session.sign ) {
+        res.send({code: 401});
+        return;
+    }
+    next();
 });
 
-//  编辑用户
-app.post('/user/updateUser', (req, res) => {
-    userModel.findByIdAndUpdate(req.body._id, req.body, err => {
-        if( !err ) {
-            res.send('用户编辑成功！');
-        } else {
-            res.status(401).send('用户编辑失败！');
-        }
-    });
-});
+app.use('/blog', require('./routers/blog'));
+app.use('/user', require('./routers/user'));
 
-//  删除用户
-app.post('/user/delUser', (req, res) => {
-    //  mongoose 建议使用delete删除  而不是remove
-    userModel.findByIdAndDelete(req.body._id, err => {
-        if( !err ) {
-            res.send('用户删除成功！');
-        } else {
-            res.status(401).send('用户删除失败！');
-        }
-    });
-});
 
 mongo.connect('mongodb://132.232.119.153:27017/vue', { useNewUrlParser: true, useFindAndModify: false }, err => {
     if( err ) {
---
+++ routers/user.js
@@ -0,0 +1,69 @@
+var router = require('express').Router();
+var userModel = require('../model/userModel');
+
+//  获取用户列表
+router.get('/getUserList', (req, res) => {
+    var pageNum = req.query.pageNum * 1 || 1;
+    var pageSize = req.query.pageSize * 1 || 10;
+    userModel.find({}).estimatedDocumentCount().then(count => {
+        if( count === 0 ) {
+            res.json({
+                users: [],
+                pageNum: 1,
+                pageSize,
+                count
+            });
+            return;
+        }
+        if( pageSize * pageNum > count ) {
+            pageNum = Math.ceil(count / pageSize);
+        }
+        userModel.find().skip((pageNum-1) * pageSize).limit(pageSize).then(users => {
+            res.json({
+                users,
+                pageNum: pageNum || 1,
+                pageSize,
+                count
+            });
+        });
+    });
+});
+
+//  添加用户
+router.post('/addUser', (req, res) => {
+    userModel.find({username: req.body.username}).then(user => {
+        if( user.length < 1 ) {
+            userModel.create(req.body, err => {
+                res.send(true);
+            });
+        } else {
+            res.send(false);
+        }
+    });
+
+});
+
+//  编辑用户
+router.post('/updateUser', (req, res) => {
+    userModel.findByIdAndUpdate(req.body._id, req.body, err => {
+        if( !err ) {
+            res.send('用户编辑成功！');
+        } else {
+            res.status(401).send('用户编辑失败！');
+        }
+    });
+});
+
+//  删除用户
+router.post('/delUser', (req, res) => {
+    //  mongoose 建议使用delete删除  而不是remove
+    userModel.findByIdAndDelete(req.body._id, err => {
+        if( !err ) {
+            res.send('用户删除成功！');
+        } else {
+            res.status(401).send('用户删除失败！');
+        }
+    });
+});
+
+module.exports = router;
