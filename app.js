22d5d620dedaa6534f7e0ab3e679a17cbdd3c993
orochitian <orochitian@126.com>
2019-03-01 15:53:25 +0800

    跨域

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var session = require('express-session');
 var history = require('connect-history-api-fallback');
 
 app.use('*', (req, res, next) => {
-    // res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     // res.header("Access-Control-Allow-Credentials", true);
